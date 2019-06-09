9130913134911a0f1150748d90d74a00f1eeea28
Cam Helkuik <cameron@travefy.com>
2019-06-09 18:57:27 -0500

    created helper for pollen value

---
+++ app/helpers/pollen-value.js
@@ -0,0 +1,20 @@
+import { helper } from '@ember/component/helper';
+
+export function pollenValue([params]/*, hash*/) {
+  switch (params){
+    case 1:
+      return "Very Low";
+    case 2:
+      return "Low";
+    case 3:
+      return "Medium";
+    case 4:
+      return "High";
+    case 5:
+      return "Very High";
+    default:
+      return "No Pollen";    
+  }
+}
+
+export default helper(pollenValue);
