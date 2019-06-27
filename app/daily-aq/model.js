6f1880440219054a3c8c7fddab58478abf97cb85
Cameron Helkuik <cameron@travefy.com>
2019-06-26 20:16:28 -0500

    Replace breezometer api (#4)
    * changing out breezeometer
    
    * officially replaced breezeometer with AccuWeather

---
+++ app/daily-aq/model.js
@@ -0,0 +1,15 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+    date: DS.attr('string'),
+    ozoneValue: DS.attr('number'),
+    ozoneCategory: DS.attr('string'),
+    grass: DS.attr('number'),
+    mold: DS.attr('number'),
+    ragweed: DS.attr('number'),
+    tree: DS.attr('number'),
+    asthmaValue: DS.attr('number'),
+    asthmaCategory: DS.attr('string'),
+    dustDanderCategory: DS.attr('string'),
+    dustDanderValue: DS.attr('number')
+});
