8cab31ffcba833e9e78a680e90d08f995ecd1d77
Cam Helkuik <cameron@travefy.com>
2019-09-08 18:47:17 -0500

    omg, forgot return in the route :see_no_evil:

--- AsthmaAlertClientNew/app/routes/tracking-items.js
+++ AsthmaAlertClientNew/app/routes/tracking-items.js
@@ -2,6 +2,6 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
     model(){
-        this.store.findAll('tracking-item');
+       return this.store.findAll('tracking-item');
     }
 });
